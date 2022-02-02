9c228adb3ce9b56d60262a183178ed85ac24dddd
admin <soinproduction@gmail.com>
2022-02-03 00:07:20 +0200

    add footer && add some scripts for buttons or catalog

--- source/js/common.js
+++ source/js/common.js
@@ -25,3 +25,52 @@ let homeSlider = new Swiper('.home-slider', {
 
 });
 
+let gridBtn = document.querySelector(".grid-button");
+let listBtn = document.querySelector(".list-button");
+let catalogBox = document.querySelector(".index-catalog-list");
+
+if (catalogBox) {
+  gridBtn.onclick = function(e) {
+    e.preventDefault();
+    this.classList.add('active');
+    listBtn.classList.remove('active');
+    catalogBox.classList.remove('row-item');
+  }
+
+  listBtn.onclick = function(e) {
+    e.preventDefault();
+    this.classList.add('active');
+    gridBtn.classList.remove('active');
+    catalogBox.classList.add('row-item');
+  }
+
+
+}
+
+
+let tabParents = document.querySelectorAll("[data-parent-tabs]");
+
+tabParents.forEach(function (tabParent) {
+  let tabNavs = tabParent.querySelectorAll("[data-tab]");
+  let tabPanes = tabParent.querySelectorAll("[data-tab-content]");
+
+  for (let i = 0; i < tabNavs.length; i++) {
+    tabNavs[i].addEventListener("click", function (e) {
+      e.preventDefault();
+      let activeTabAttr = e.target.getAttribute("data-tab");
+
+      for (let j = 0; j < tabNavs.length; j++) {
+        let contentAttr = tabPanes[j].getAttribute("data-tab-content");
+
+        if (activeTabAttr === contentAttr) {
+          tabNavs[j].classList.add("active");
+          tabPanes[j].classList.add("active");
+        } else {
+          tabNavs[j].classList.remove("active");
+          tabPanes[j].classList.remove("active");
+        }
+      };
+    });
+  }
+
+});
